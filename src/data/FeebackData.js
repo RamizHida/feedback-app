37f33cc8c7337ea77719ff3b82eac845051cef60
Ramiz Hida <ramizhida00@gmail.com>
2022-12-17 17:04:41 +0100

    Feedback App

---
+++ src/data/FeebackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nam est natus deserunt minus pariatur eius molestiae quibusdam quisquam alias",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nam est natus deserunt minus pariatur eius molestiae quibusdam quisquam alias",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nam est natus deserunt minus pariatur eius molestiae quibusdam quisquam alias",
+  },
+];
+
+export default FeedbackData;
